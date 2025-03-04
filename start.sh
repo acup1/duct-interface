efc81d1979793ea1513a272e90db0df1d2af8729
A cup <georgiyegoriy@gmail.com>
2025-03-04 12:02:25 +0300

    qwe

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 #sleep 5
 cd /home/orangepi/duct-interface/
 rm -f .git/index
-git clean -xdf
-git reset --hard
+git reset
+#git clean -xdf
+#git reset --hard
 git pull
 cd /home/orangepi/duct-interface/interface
 python3 main.py
